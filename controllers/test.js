b34ac9767c955e2878bfa543c408ebc32f365944
alee90 <adlee90@gmail.com>
2016-06-18 17:52:47 -0400

    another commit

--- controllers/test.js
+++ controllers/test.js
@@ -1,6 +1,43 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var clientID = 'Client-ID ' + process.env.IMGUR_CLIENT_ID
+
+console.log('hello')
+
+var nytkey = process.env.NYT_KEY;
+
+router.get('/', function(req, res){
+    var options = {
+        "url": "https://api.nytimes.com/svc/topstories/v2/home.json",
+        qs: {
+            "api-key": process.env.NYT_KEY}
+     };
+	request(options, function (error, response, body) {
+		if(!error && response.statusCode == 200){
+			var data = JSON.parse(body);
+			res.send(data);
+		}
+
+	})
+
+});
+
+router.get('/pics/forks', function(req, res){
+	var options = {
+		'url': "https://api.imgur.com/3/gallery/search/?q_exactly=forks",
+	  headers: {
+    	'Authorization': clientID
+  	}
+	};
+	request(options, function (error, response, body) {
+		if(!error && response.statusCode == 200){
+			console.log('hat');
+			var bodied = JSON.parse(body);
+			res.send(bodied);
+		}
+	})
+});
 
 
 var openWeatherId = process.env.OPEN_WEATHER_ID;
