69dd1c7fbcc20935afd584894ac6305afd2fdad5
Shaked Edri <ShakedEdri@Shakeds-MacBook-Pro.local>
2016-06-18 17:49:53 -0400

    pre pull

--- controllers/users.js
+++ controllers/users.js
@@ -1,29 +1,29 @@
 var express = require('express');
 var router = express.Router();
-// var passport = require('../config/passport.js');
-// var User = require('../models/users.js');
-// var Notes = require('../models/notes.js');
-// var request = require('request');
-// console.log(clientID);
+var passport = require('../config/passport.js');
+var User = require('../models/users.js');
+var Notes = require('../models/notes.js');
+var request = require('request');
+console.log(clientID);
 
 
 
-//!!======== NO AUTH ========!!\\
+// !!======== NO AUTH ========!!\\
 
-// // CREATE A NEW USER
-// router.post('/', function(req, res) {
-// 	User.create(req.body, function(err, user) {
-// 		if(err) {
-// 			console.log(err); 
-// 			res.status(500).end();
-// 		}
-// 		res.send(true);
-// 	});
-// });
+// CREATE A NEW USER
+router.post('/', function(req, res) {
+	User.create(req.body, function(err, user) {
+		if(err) {
+			console.log(err); 
+			res.status(500).end();
+		}
+		res.send(true);
+	});
+});
 
-//!!======== AUTH ========!!\\
+// !!======== AUTH ========!!\\
 
-// router.use(passport.authenticate('jwt', { session: false }));
+router.use(passport.authenticate('jwt', { session: false }));
 
 
 	
