3d6d626981b7d45b33d5b1a0c5ecfec3b9a5fba2
alee90 <adlee90@gmail.com>
2016-06-18 15:17:19 -0400

    added backend logic for future react ajax

--- controllers/test.js
+++ controllers/test.js
@@ -4,6 +4,9 @@ var request = require('request');
 
 
 var openWeatherId = process.env.OPEN_WEATHER_ID;
+var clientID = 'Client-ID ' + process.env.IMGUR_CLIENT_ID
+console.log('hello')
+var nytkey = process.env.NYT_KEY;
 
 
 router.get('/weather/', function(req, res){
@@ -14,6 +17,37 @@ router.get('/weather/', function(req, res){
     })
 })
 
+router.get('/', function(req, res){
+    var options = {
+        "url": "https://api.nytimes.com/svc/topstories/v2/home.json",
+        qs: {
+            "api-key": process.env.NYT_KEY}
+     };
+	request(options, function (error, response, body) {
+		if(!error && response.statusCode == 200){
+			var data = JSON.parse(body);
+			res.send(data);
+		}
+
+	})
+
+});
+
+router.get('/pics/forks', function(req, res){
+	var options = {
+		'url': "https://api.imgur.com/3/gallery/search/?q_exactly=forks",
+	  headers: {
+    	'Authorization': clientID
+  	}
+	};
+	request(options, function (error, response, body) {
+		if(!error && response.statusCode == 200){
+			console.log('hat');
+			var bodied = JSON.parse(body);
+			res.send(bodied);
+		}
+	})
+});
 
 
 module.exports = router;
--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-
-
 ///Main component
 
 var App = React.createClass({
